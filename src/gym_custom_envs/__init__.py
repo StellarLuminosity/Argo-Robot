47d03c95d60d45d920b4410f8c9ef6eb8338e6df
Federico Sarrocco <fede.sarrocco@gmail.com>
2025-01-19 16:29:01 +0100

    [src/gym_custom_envs, src/examples]: remove deprecated custom environment and example scripts

--- src/gym_custom_envs/__init__.py
+++
@@ -1,9 +0,0 @@
-from gymnasium.envs.registration import register
-from gym_custom_envs.o2_env import AntEnv
-
-register(
-    id='o2-v0',
-    entry_point='gym_custom_envs.o2_env:AntEnv',
-    disable_env_checker = True,
-    order_enforce=False
-)
